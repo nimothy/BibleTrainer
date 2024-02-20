134f77bacc3242bf92a43a86701083cb627312c1
James Doc <hi@jamesdoc.com>
2024-02-20 16:33:01 +0000

    Flutter Hello World

---
+++ android/app/src/main/kotlin/com/example/bibletrainer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bibletrainer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const ENV = {
-  production: false,
-  esvApiKey: 'Token 332b2b26bf6328da3e8d2b4aaf99155600668fcc'
-};
